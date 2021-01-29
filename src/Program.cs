6c3f0d327125a2600e58d06a1387990dc40cfe5d
Marcel <dolly.marcel@gmail.com>
2021-01-29 00:13:10 -0300

    Add placeholder Program.cs

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Nu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jointArgs = string.Join(',', args);
+            Console.WriteLine($"Hello World {jointArgs}!");
+        }
+    }
+}

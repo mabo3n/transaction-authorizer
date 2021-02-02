0fac1085e744e0f5af6cf65be9355b20eee7d57c
Marcel <dolly.marcel@gmail.com>
2021-02-02 01:34:29 -0300

    Add repo dependencies to operation handlers

--- src/Application/AuthorizeTransaction/AuthorizeTransactionHandler.cs
+++ src/Application/AuthorizeTransaction/AuthorizeTransactionHandler.cs
@@ -2,6 +2,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using Authorizer.Domain.Entities;
 using Authorizer.Domain.Enumerations;
+using Authorizer.Domain.Repositories;
+using Authorizer.Domain.Services;
 using MediatR;
 
 namespace Authorizer.Application
@@ -9,6 +11,18 @@ namespace Authorizer.Application
     public class AuthorizeTransactionHandler
         : IRequestHandler<AuthorizeTransaction, OperationResult>
     {
+        private readonly ITransactionRepository transactionRepository;
+        private readonly ITransactionService transactionService;
+
+        public AuthorizeTransactionHandler(
+            ITransactionRepository transactionRepository,
+            ITransactionService transactionService
+        )
+        {
+            this.transactionRepository = transactionRepository;
+            this.transactionService = transactionService;
+        }
+
         public Task<OperationResult> Handle(
             AuthorizeTransaction payload,
             CancellationToken cancellationToken
--- src/Application/CreateAccount/CreateAccountHandler.cs
+++ src/Application/CreateAccount/CreateAccountHandler.cs
@@ -2,6 +2,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Authorizer.Domain.Entities;
 using Authorizer.Domain.Enumerations;
+using Authorizer.Domain.Repositories;
 using MediatR;
 
 namespace Authorizer.Application
@@ -9,6 +10,13 @@ namespace Authorizer.Application
     public class CreateAccountHandler
         : IRequestHandler<CreateAccount, OperationResult>
     {
+        private readonly IAccountRepository accountRepository;
+
+        public CreateAccountHandler(IAccountRepository accountRepository)
+        {
+            this.accountRepository = accountRepository;
+        }
+
         public Task<OperationResult> Handle(
             CreateAccount payload,
             CancellationToken cancellationToken

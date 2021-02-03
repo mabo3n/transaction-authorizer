03b3951beb1efb3edc10f428a54df4121b1a8728
Marcel <dolly.marcel@gmail.com>
2021-02-03 00:25:25 -0300

    Fix transaction service tests

--- tests/Domain/Services/TransactionServiceTests.cs
+++ tests/Domain/Services/TransactionServiceTests.cs
@@ -24,26 +24,41 @@ namespace AuthorizerTests.Domain.Services
             service = new TransactionService(transactionRepository);
         }
 
-        private void GenerateRandomTransactions(
+        private void PerformTransaction(Transaction transaction)
+        {
+            dataSource.Transactions = dataSource.Transactions
+                .Append(transaction);
+            dataSource.Account = dataSource.Account
+                .Apply(transaction);
+        }
+
+        private void PerformRandomTransactions(
             DateTime from, DateTime to, params int[] amounts
         )
-            => dataSource.Transactions = dataSource.Transactions
-                .Concat(
-                    amounts.Select(
-                        amount => new Transaction(
-                            merchant: $"Merchant {DateTime.Now.Ticks.ToString()}",
-                            amount,
-                            time: DateTimeUtils.RandomBetween(
-                                from, to, randomizer: random
-                            )
-                        )
+        {
+            var transactions = amounts.Select(
+                amount => new Transaction(
+                    merchant: $"Merchant {DateTime.Now.Ticks.ToString()}",
+                    amount,
+                    time: DateTimeUtils.RandomBetween(
+                        from, to, randomizer: random
                     )
-                );
+                )
+            );
+
+            dataSource.Account = transactions.Aggregate(
+                dataSource.Account,
+                (account, transaction) => account.Apply(transaction)
+            );
+
+            dataSource.Transactions = dataSource.Transactions
+                .Concat(transactions);
+        }
 
         [Fact]
         public void Authorize_ShouldReturnNoViolation()
         {
-            var account = new Account(availableLimit: 100, activeCard: true);
+            var account = new Account(availableLimit: 1000, activeCard: true);
 
             var someTransaction = new Transaction(
                 merchant: "Some merchant", amount: 200, time: 05.February(2021)
@@ -53,29 +68,6 @@ namespace AuthorizerTests.Domain.Services
 
             violations
                 .Should().BeEmpty();
-        }
-
-        [Fact]
-        public void Authorize_ShouldReturnException_WhenAccountIsNull()
-        {
-            Account account = null;
-
-            var someTransaction = new Transaction(
-                merchant: "Some merchant", amount: 200, time: 05.February(2021)
-            );
-
-            var violations = service.Authorize(someTransaction, account);
-
-            var exception = Record.Exception(
-                () => account.Apply(someTransaction)
-            );
-
-            exception
-                .Should()
-                .BeOfType<ArgumentException>()
-                .Which.Message
-                .Should()
-                .MatchEquivalentOf("*account*");
         }
 
         [Fact]
@@ -90,16 +82,16 @@ namespace AuthorizerTests.Domain.Services
             var violations = service.Authorize(someTransaction, account);
 
             violations
-                .Should().HaveCount(1)
-                .And
-                .Contain(Violation.CardNotActive);
+                .Should().BeEquivalentTo(
+                    new[] { Violation.CardNotActive }
+                );
         }
 
         [Fact]
         public void Authorize_ShouldReturnViolation_WhenAccountHasInsufficientLimit()
         {
             var account = new Account(
-                availableLimit: 380, activeCard: false
+                availableLimit: 380, activeCard: true
             );
 
             var transaction = new Transaction(
@@ -109,29 +101,33 @@ namespace AuthorizerTests.Domain.Services
             var violations = service.Authorize(transaction, account);
 
             violations
-                .Should().HaveCount(1)
-                .And
-                .Contain(Violation.InsufficientLimit);
+                .Should().BeEquivalentTo(
+                    new[] { Violation.InsufficientLimit }
+                );
         }
 
         [Fact]
         public void Authorize_ShouldReturnViolation_WhenTransactionsExceedThreeInATwoMinuteInterval()
         {
-            var account = new Account(availableLimit: 1000, activeCard: true);
+            dataSource.Account = new Account(
+                availableLimit: 1000, activeCard: true
+            );
 
             DateTime now = 5.February(2021);
             DateTime minutesAgo(int minutes) => now.AddMinutes(-minutes);
             DateTime someHoursAgo = minutesAgo(60 * 3);
 
-            GenerateRandomTransactions(
+            PerformRandomTransactions(
                 from: someHoursAgo, to: minutesAgo(2),
                 30, 20, 10, 20, 30
             );
 
-            GenerateRandomTransactions(
+            PerformRandomTransactions(
                 from: minutesAgo(2), to: now,
                 25, 25, 25
             );
+
+            var account = dataSource.Account;
 
             var transaction = new Transaction(
                 merchant: "Some merchant", amount: 10, time: now
@@ -140,15 +136,17 @@ namespace AuthorizerTests.Domain.Services
             var violations = service.Authorize(transaction, account);
 
             violations
-                .Should().HaveCount(1)
-                .And
-                .Contain(Violation.HighFrequencySmallInterval);
+                .Should().BeEquivalentTo(
+                    new[] { Violation.HighFrequencySmallInterval }
+                );
         }
 
         [Fact]
         public void Authorize_ShouldReturnViolation_WhenTransactionIsDoubledInATwoMinuteInterval()
         {
-            var account = new Account(availableLimit: 1000, activeCard: true);
+            dataSource.Account = new Account(
+                availableLimit: 1000, activeCard: true
+            );
 
             var merchant = "BurgBurgBurg";
             var amount = 123;
@@ -157,34 +155,37 @@ namespace AuthorizerTests.Domain.Services
             DateTime minutesAgo(int minutes) => now.AddMinutes(-minutes);
             DateTime someHoursAgo = minutesAgo(60 * 3);
 
-            GenerateRandomTransactions(
+            PerformRandomTransactions(
                 from: someHoursAgo, to: minutesAgo(2),
                 10, 20, 30
             );
 
-            dataSource.Transactions = dataSource.Transactions.Append(
-                new Transaction(merchant, amount, time: minutesAgo(1))
-            );
+            PerformTransaction(new Transaction(
+                merchant, amount, time: minutesAgo(1)
+            ));
 
-            GenerateRandomTransactions(
+            PerformRandomTransactions(
                 from: minutesAgo(1), to: now,
                 25
             );
 
+            var account = dataSource.Account;
             var doubledTransaction = new Transaction(merchant, amount, time: now);
 
             var violations = service.Authorize(doubledTransaction, account);
 
             violations
-                .Should().HaveCount(1)
-                .And
-                .Contain(Violation.DoubledTransaction);
+                .Should().BeEquivalentTo(
+                    new[] { Violation.DoubledTransaction }
+                );
         }
 
         [Fact]
         public void Authorize_ShouldNotReturnViolation_WhenTransactionIsDoubledAfterATwoMinuteInterval()
         {
-            var account = new Account(availableLimit: 1000, activeCard: true);
+            dataSource.Account = new Account(
+                availableLimit: 1000, activeCard: true
+            );
 
             var merchant = "BurgBurgBurg";
             var amount = 123;
@@ -193,20 +194,21 @@ namespace AuthorizerTests.Domain.Services
             DateTime minutesAgo(int minutes) => now.AddMinutes(-minutes);
             DateTime someHoursAgo = minutesAgo(60 * 3);
 
-            GenerateRandomTransactions(
+            PerformRandomTransactions(
                 from: someHoursAgo, to: minutesAgo(3),
                 10, 20, 30
             );
 
-            dataSource.Transactions = dataSource.Transactions.Append(
-                new Transaction(merchant, amount, time: minutesAgo(3))
-            );
+            PerformTransaction(new Transaction(
+                merchant, amount, time: minutesAgo(3)
+            ));
 
-            GenerateRandomTransactions(
+            PerformRandomTransactions(
                 from: minutesAgo(1), to: now,
                 25
             );
 
+            var account = dataSource.Account;
             var transaction = new Transaction(merchant, amount, time: now);
 
             var violations = service.Authorize(transaction, account);
@@ -218,7 +220,9 @@ namespace AuthorizerTests.Domain.Services
         [Fact]
         public void Authorize_ShouldReturnMultipleViolations_WhenTransactionIsDoubledAndExceedsAccountLimitAndTransactionQuantityLimit()
         {
-            var account = new Account(availableLimit: 1000, activeCard: true);
+            dataSource.Account = new Account(
+                availableLimit: 1000, activeCard: true
+            );
 
             var merchant = "BurgBurgBurg";
             var amount = 300;
@@ -227,28 +231,27 @@ namespace AuthorizerTests.Domain.Services
             DateTime minutesAgo(int minutes) => now.AddMinutes(-minutes);
             DateTime someHoursAgo = minutesAgo(60 * 3);
 
-            GenerateRandomTransactions(
+            PerformRandomTransactions(
                 from: someHoursAgo, to: minutesAgo(1),
                 200, 100, 200
             );
 
-            dataSource.Transactions = dataSource.Transactions.Append(
-                new Transaction(merchant, amount, time: minutesAgo(1))
-            );
+            PerformTransaction(new Transaction(
+                merchant, amount, time: minutesAgo(1)
+            ));
 
-            GenerateRandomTransactions(
+            PerformRandomTransactions(
                 from: minutesAgo(1), to: now,
                 100, 100
             );
 
+            var account = dataSource.Account;
             var transaction = new Transaction(merchant, amount, time: now);
 
             var violations = service.Authorize(transaction, account);
 
             violations
-                .Should().HaveCount(3)
-                .And
-                .BeEquivalentTo(new[] {
+                .Should().BeEquivalentTo(new[] {
                     Violation.DoubledTransaction,
                     Violation.HighFrequencySmallInterval,
                     Violation.InsufficientLimit,
